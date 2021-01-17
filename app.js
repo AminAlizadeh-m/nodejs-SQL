5e85576e26c6cce11c7f9f14484da3b7751ec839
aminalizadeh.developer <aminalizadeh.developer@gmail.com>
2021-01-17 23:25:06 +0330

    send all data of table as response

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
 
-// app.get('/', (req, res, next) => {
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodejs',
+    port: 3307
+})
 
-// });
+// READ FROM SQL DATABASE
+app.get('/', (req, res, next) => {
+    pool.getConnection((err, connectin) => {
+        if (err) {
+            throw err;
+        }
+
+        connectin.query('SELECT * FROM my_table', (error, result) => {
+            if (error) {
+                return res.status(401).json({
+                    message: 'Some thing went wrong'
+                });
+            }
+
+            res.status(200).send(result);
+        });
+    });
+});
 
 module.exports = app;
